dbf1a39cad73bca61b1d563b5fd3447b9f4e3e7f
Kishor Kharade <kishor.kharade@atmecs.com>
2017-11-21 14:55:05 +0530

    Showing recipe details dynamcally

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,10 +8,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is desc',
       'https://c.pxhere.com/photos/50/03/beirut_power_supply_snack-1375814.jpg!d'),
-    new Recipe('A Test Recipe', 'This is desc',
+    new Recipe('Another Test Recipe', 'This is desc',
       'https://c.pxhere.com/photos/50/03/beirut_power_supply_snack-1375814.jpg!d')
   ];
 
@@ -20,4 +23,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
